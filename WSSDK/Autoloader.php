dd5c2acf98d82d19a147c21256afc441c87585f5
Mario Stefanov <stefanovn19@yahoo.co.uk>
2018-03-29 20:36:33 +0300

    CreditCard Update

--- WSSDK/Autoloader.php
+++ WSSDK/Autoloader.php
@@ -5,14 +5,6 @@ class WSSDK_Autoloader
     public static function autoloader($class)
     {
         require_once('WSSDK.php');
-//        if (strpos($class, 'WSSDK') === 0) {
-//            $className = str_replace('_', '/', $class);
-//            $className = substr($className, 12);
-//
-//            if (is_file($classFile = dirname(__FILE__) . '/'. $className.'.php')) {
-//                require_once($classFile);
-//            }
-//        }
     }
 
     public static function register()
